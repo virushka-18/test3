3706c0d1f7ff1579e14620f1020568cc60d5e86c
Gopichand <j.gopichandyadav@gmail.com>
2025-03-12 11:07:32 +0530

    First Message

---
+++ src/test/java/com/test3/test3/Test3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test3.test3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
